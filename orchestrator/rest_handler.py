1948e8164e2e10367121f00f3d00fa73abd93e7d
mixy <merzlikinmihail29@gmail.com>
2026-04-08 14:29:15 +0300

    api method fix

--- orchestrator/rest_handler.py
+++ orchestrator/rest_handler.py
@@ -1,4 +1,3 @@
-from orchestrator.main import api_app
 from orchestrator.dto import (
     CreateCourseRequest,
     CreateCourseResponse,
@@ -9,21 +8,24 @@ from orchestrator.dto import (
     GetTopicRequest,
     GetTopicResponse,
 )
+from orchestrator.main import api_app
 
-@api_app.post("/get_graphs", response_model=GetGraphsResponse)
+
+@api_app.get("/get_graphs", response_model=GetGraphsResponse)
 async def get_graph_api(request: GetGraphsRequest):
     return await api_app.state.app.get_graphs(request)
 
 
-@api_app.post("/get_topic", response_model=GetTopicResponse)
+@api_app.get("/get_topic", response_model=GetTopicResponse)
 async def get_topic_api(request: GetTopicRequest):
     return await api_app.state.app.get_topic(request)
+
 
 @api_app.post("/create_new_course", response_model=CreateCourseResponse)
 async def new_course_api(request: CreateCourseRequest):
     return await api_app.state.app.create_new_course(request)
 
 
-@api_app.post("/get_graph_previews", response_model=GetGraphsPreviewResponse)
+@api_app.get("/get_graph_previews", response_model=GetGraphsPreviewResponse)
 async def get_graph_previews_api(request: GetGraphsPreviewRequest):
     return await api_app.state.app.get_graph_previews(request)
