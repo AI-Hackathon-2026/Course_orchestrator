2bda85b8bf459bb6de2667fffcb11b827e353ea3
IMixyI <merzlikinmihail29@gmail.com>
2026-04-20 15:16:19 +0300

    Update config.py

--- orchestrator/config.py
+++ orchestrator/config.py
@@ -8,7 +8,7 @@ class BackendConfig(BaseSettings):
 
 
 class MongoDBConfig(BaseSettings):
-    MONGO_HOST: str = "localhost"
+    MONGO_HOST: str = "mongo_db"
     PORT: int = 27017
     MONGO_URL: str = f"mongodb://{MONGO_HOST}:{PORT}/"
     DATABASE: str = "courses"
